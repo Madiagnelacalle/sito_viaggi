e3d3ae3849fa7207702cabccfa6e7553e8b0c691
Guisse-Aboubakri <guisse.aboubakri.05@itisdalmine.edu.it>
2025-03-10 22:01:49 +0100

    visualizza e scheda

--- sito.py
+++ sito.py
@@ -1,6 +1,19 @@
 import os
 
-viaggi = {}
+volo_1 = {
+    "ID": 300,
+    "nome": "primo",
+    "destinazione": "roma",
+    "data": 250720,
+}
+volo_2 = {
+    "ID": 120,
+    "nome": "secondo",
+    "destinazione": "napoli",
+    "data": 251201,
+}
+
+viaggi= [volo_1, volo_2]
 
 def aggiungi_viaggio():
     id_viaggio = input("Inserisci ID viaggio: ")
@@ -40,3 +53,21 @@ def modifica_viaggio():
         viaggi[id_viaggio]["data"] = data
     
     return f"Viaggio modificato: {viaggi[id_viaggio]}"
+
+def visualizza_viaggio(lista):
+    id_viaggio = int(input("Inserire ID del viaggio da visualizzare: "))
+    count = 0
+    while count < len(lista):
+        if id_viaggio == lista[count]["ID"]:
+            print(f"ID: {lista[count]['ID']}\nNome: {lista[count]['nome']}\nDestinazione: {lista[count]['destinazione']}\nData: {lista[count]['data']}")
+            return
+        count +=1
+    print("ID del viaggio non trovato")
+    return -1
+    
+def scheda_viaggi(lista):
+    count = 0
+    while count < len(lista):
+        print(f"{lista[count]}")
+        count +=1
+    return
